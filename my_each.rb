9f3253ab75e596dcc85ebe907055e36ac5c2e3ed
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-12 00:27:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,8 @@
 def my_each(collection)
   result = []
   i = 0 
-  result << yield(collection[i])
+  while i < collection.length 
+    result << yield(collection[i])
   # put argument(s) here
   # code here
 end
